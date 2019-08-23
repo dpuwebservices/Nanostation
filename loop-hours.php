ac8cecede88fc0a5a0c2917da4c20bef04036356
Christine McClure <camcclure@gmail.com>
2019-08-23 10:12:55 -0500

    remove unwanted elements; enlarge wanted elements

--- loop-hours.php
+++ loop-hours.php
@@ -11,8 +11,8 @@
     body, html{
 
       font-family:"Open Sans Condensed",sans-serif;
-      font-size: 1.1em;
       background: #161a55;
+/*      background: red;*/
       color:white;
       text-align:left;
       overflow:hidden;
@@ -20,8 +20,8 @@
     
     a {color:white; text-decoration: none;}
     #main{
-      max-width: 90%;
-      padding:6% 0 4% 0;
+      max-width: 100%;
+      padding:4%;
       margin:0 auto;
     }
     
@@ -38,23 +38,28 @@
       font-weight:bold;
     }
     
+    tr.dep2440, th.hours-col-loc{display:none;}
+    
+    .date{font-size: 2em;}
+    .hours{font-size: 1.8em;}
+    
 
   </style>
 
 </head>
 <?php 
 date_default_timezone_set('America/Chicago');
-$todaysDate=date("l, F jS");
+$todaysDate=date("l, M. jS");
 ?>
 <body>
   <div id="main">
 
     
     <table>
-      <tr><td><strong><?php echo $todaysDate; ?></strong></td></tr>
+      <tr><td class="date"><?php echo $todaysDate; ?></td></tr>
       <tr><td>
-          <p>Today's Hours:</p>
-          <div id="api_hours_today_iid123_lid2438"></div>
+          <h2>Today's Hours:</h2>
+          <div class="hours" id="api_hours_today_iid123_lid2438"></div>
         </td></tr>
     </table>
   
@@ -62,11 +67,6 @@ $todaysDate=date("l, F jS");
 
   <script src="https://api3.libcal.com/api_hours_today.php?iid=123&lid=2438&format=js&systemTime=0&context=object"> </script>
   </div>
-<!--  <h2>Room Reservations</h2>
 
-
-  <h2>Instruction Sessions</h2>
-<div id="api_upc_cid2894_iid123"></div><script type="text/javascript" src="https://api3.libcal.com/api_events.php?iid=123&m=upc&cid=2894&audience=&c=1652&d=23632&l=3&simple=ul_date&context=object&format=js"> </script>
-  </div>-->
 </body>
 </html>
